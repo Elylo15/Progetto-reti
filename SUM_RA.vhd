50ba6e7addcc96b1ed5e2afc4637495fc6bcba9c
Elylo <58746092+Elylo15@users.noreply.github.com>
2024-03-21 14:47:24 +0100

    Update SUM_RA.vhd

--- SUM_RA.vhd
+++ SUM_RA.vhd
@@ -12,7 +12,8 @@ end SUM_RA;
 architecture SUM_RA_arch of SUM_RA is
     signal store: std_logic_vector (15 downto 0);
     signal sum: std_logic_vector(15 downto 0);
-    signal carry: std_logic :='0';
+    signal carry_in: std_logic :='0';
+    signal carry_out: std_logic :='0';
     component ha
         port(
             a: in std_logic;
@@ -22,9 +23,10 @@ architecture SUM_RA_arch of SUM_RA is
         );
     end component;
 begin
-    HA_0: ha port map ((ADD_EN and '1'), RA(0),sum(0), carry);
+    HA_0: ha port map ((ADD_EN and '1'), RA(0),sum(0), carry_in);
     ADDS_generator: for I in 1 to 15 generate
-        HA_I: ha port map (RA(I-1), RA(I), sum(0), carry);
+        HA_I: ha port map (carry_in, RA(I), sum(0), carry_out);
+        carry_in <=carry_out;
     end generate;
     store<=RA;
     output <= store;
