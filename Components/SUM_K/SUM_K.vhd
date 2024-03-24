f6cd38c9b44db3c0a0745f65e09013730e51dd78
Elylo <58746092+Elylo15@users.noreply.github.com>
2024-03-24 17:14:33 +0100

    Update SUM_K.vhd

--- Components/SUM_K/SUM_K.vhd
+++ Components/SUM_K/SUM_K.vhd
@@ -12,7 +12,9 @@ end SUM_K;
 architecture SUM_K_arch of SUM_K is
     signal store: std_logic_vector (9 downto 0);
     signal sum: std_logic_vector(9 downto 0);
-    signal carry: std_logic :='0';
+     signal carry_in: std_logic :='0';
+    signal carry_out: std_logic :='0';
+    signal AND_K: std_logic := K_EN and '1';
     component ha_k
         port(
             a: in std_logic;
@@ -22,9 +24,10 @@ architecture SUM_K_arch of SUM_K is
         );
     end component;
 begin
-    HA_0: ha_k port map ((K_EN and '1'), RK(0),sum(0), carry);
+    HA_0: ha_k port map (AND_K, RK(0),sum(0), carry_in);
     ADDS_generator: for I in 1 to 8 generate
-        HA_I: ha_k port map (RK(I-1), RK(I), sum(0), carry);
+        HA_I: ha_k port map (carry_in, RK(I), sum(0), carry_out);
+        carry_in<= carry_out;
     end generate;
     store<=RK;
     output <= store;
