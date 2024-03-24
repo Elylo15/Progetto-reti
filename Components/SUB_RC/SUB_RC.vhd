ffb7e7a14f2b0caf9ccff480b43b8855dbda0250
Elylo <58746092+Elylo15@users.noreply.github.com>
2024-03-24 17:13:37 +0100

    Update SUB_RC.vhd

--- Components/SUB_RC/SUB_RC.vhd
+++ Components/SUB_RC/SUB_RC.vhd
@@ -4,17 +4,18 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity SUB_RC is
     Port (
-        RC: in std_logic_vector(15 downto 0);
+        RC: in std_logic_vector(7 downto 0);
         SUB_EN: in std_logic;
-        output: out std_logic_vector(15 downto 0)
+        output: out std_logic_vector(7 downto 0)
     );
 end SUB_RC;
 
 architecture SUB_RC_arch of SUB_RC is
-    signal store: std_logic_vector (15 downto 0);
-    signal diff: std_logic_vector(15 downto 0);
-    signal borrow: std_logic :='0';
-
+    signal store: std_logic_vector (7 downto 0);
+    signal diff: std_logic_vector(7 downto 0);
+    signal borrow_in: std_logic :='0';
+    signal borrow_out: std_logic :='0';
+    signal AND_RC: std_logic := SUB_EN and '1';
     component hs  is
         Port (
             a: in std_logic;
@@ -24,9 +25,10 @@ architecture SUB_RC_arch of SUB_RC is
         );
     end component;
 begin
-    HS_0: hs port map ((SUB_EN and '1'), RC(0),borrow, diff(0));
+    HS_0: hs port map (AND_RC, RC(0),borrow_in, diff(0));
     ADDS_generator: for I in 1 to 7 generate
-        HS_I: hs port map (RC(I-1), RC(I), borrow, diff(0));
+        HS_I: hs port map (borrow_in, RC(I), borrow_out, diff(0));
+        borrow_in <= borrow_out;
     end generate;
     store<=RC;
     output <= store;
