80c424cfe493c0a9303e20c6dd55c0d73a94129c
Elylo <58746092+Elylo15@users.noreply.github.com>
2024-03-28 14:06:40 +0100

    Update multiplexer_o_mem_data.vhd

--- Components/multiplexer/multiplexer_o_mem_data.vhd
+++ Components/multiplexer/multiplexer_o_mem_data.vhd
@@ -2,18 +2,28 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity multiplexer_o_mem_data is
-port(
-		RC: in std_logic_vector(7 downto 0);
+    port(
+        RC: in std_logic_vector(7 downto 0);
         RD: in std_logic_vector(7 downto 0);
-		SEL_OUT: in std_logic;
-		o_mem_data:	out std_logic_vector(7 downto 0)
-	);
+        SEL_OUT: in std_logic;
+        o_mem_data:	out std_logic_vector(7 downto 0)
+    );
 end multiplexer_o_mem_data;
 
 architecture multiplexer_o_mem_data_arch of multiplexer_o_mem_data is
 
 begin
     --quando sel =0, O_MEM_DATA = RC
-	--quando sel =1, O_MEM_DATA = RD
-	o_mem_data <= RC when SEL_OUT = '0' else RD;
+    --quando sel =1, O_MEM_DATA = RD
+    process (SEL_OUT, RC, RD)
+    begin
+        if SEL_OUT = '0' then
+            o_mem_data <= RC;
+        elsif SEL_OUT = '1' then
+            o_mem_data <= RD;
+        else
+            -- Default case if SEL_OUT is not 0 or 1
+            o_mem_data <= (others => 'X'); -- Output undetermined
+        end if;
+    end process;
 end multiplexer_o_mem_data_arch;
