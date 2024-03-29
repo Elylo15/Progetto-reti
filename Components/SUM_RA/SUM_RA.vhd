b42a95ea02ac9e188e756e9907a8965be93aaf37
Elylo <58746092+Elylo15@users.noreply.github.com>
2024-03-29 16:26:45 +0100

    Update SUM_RA.vhd

--- Components/SUM_RA/SUM_RA.vhd
+++ Components/SUM_RA/SUM_RA.vhd
@@ -10,25 +10,26 @@ entity SUM_RA is
 end SUM_RA;
 
 architecture SUM_RA_arch of SUM_RA is
-    signal store: std_logic_vector (15 downto 0);
-    signal sum: std_logic_vector(15 downto 0);
-    signal AND_ADDRESS: std_logic := ADD_EN and '1';
-    signal carry_in: std_logic :='0';
-    signal carry_out: std_logic :='0';
-    component ha
-        port(
-            a: in std_logic;
-            b: in std_logic;
-            c: out std_logic;
-            s: out std_logic
-        );
-    end component;
-begin
-    HA_0: ha port map (AND_ADDRESS, RA(0),sum(0), carry_in);
-    ADDS_generator: for I in 1 to 15 generate
-        HA_I: ha port map (carry_in, RA(I), sum(0), carry_out);
-        carry_in <=carry_out;
-    end generate;
-    store<=RA;
-    output <= store;
+
+   signal carry: std_logic_vector(15 downto 0);
+ begin   
+    HA0: entity work.half_adder
+        port map(
+            a => RA(0),
+		    b => ADD_EN,
+		    c => carry(0),
+		    s => output(0)
+    );
+    
+    GEN_HALF_ADDERS: for i in 1 to 15 generate 
+        HA_i: entity work.half_adder
+            port map(
+               a => RA(i),
+		       b => carry(i-1),
+		       c => carry(i),
+		       s => output(i)
+    );
+    end generate GEN_HALF_ADDERS;
+    
+
 end SUM_RA_arch;
