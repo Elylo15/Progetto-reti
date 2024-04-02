e24d513ca8d80a02b1593b14264a9868e485d3d8
Francesco Marchi <162024501+francesco1marchi@users.noreply.github.com>
2024-04-02 20:26:06 +0200

    Update FSM
    Aggiunto uno stato SX e un segnale SEL_ADD: adesso la macchina gestirà la selezione tra i_add e il REG_ADD calcolato.

--- Components/FSM/FSM.vhd
+++ Components/FSM/FSM.vhd
@@ -9,7 +9,7 @@ entity FSM is
 end entity;
 
 architecture FSM_arch of FSM is
-    type S is (S0, S1, S2, S3, S4, S5, S6, S7, S8, S9, SF);
+    type S is (S0, S1, S2, S3, S4, S5, S6, S7, S8, S9, SF, SX);
     signal curr_state: S;
 
 begin
@@ -21,6 +21,8 @@ begin
             if (curr_state=S0 and START='0' and DONE='0') then
                 curr_state <= S0;
             elsif (curr_state=S0 and START='1' and DONE='0') then
+                curr_state <= SX;
+            elsif (curr_state=SX) then
                 curr_state <= S1;
             elsif (curr_state=S1 and START='1' and E='0' and DONE='0') then
                 curr_state <= S2;
@@ -63,6 +65,7 @@ begin
 end process;
 with curr_state select
             ADD_EN <= '0' when S0,
+                      '0' when SX,
                       '1' when S1,
                       '1' when S2,
                       '1' when S3,
@@ -76,6 +79,7 @@ with curr_state select
                       'X' when others;
         with curr_state select
             RD_EN <=  '0' when S0,
+                      '0' when SX,
                       '0' when S1,
                       '1' when S2,
                       '0' when S3,
@@ -89,6 +93,7 @@ with curr_state select
                       'X' when others;
         with curr_state select
            SEL_OUT <= '0' when S0,
+                      '0' when SX,
                       '0' when S1,
                       '0' when S2,
                       '1' when S3,
@@ -102,6 +107,7 @@ with curr_state select
                       'X' when others;
         with curr_state select
             RC_RST <= '1' when S0,
+                      '0' when SX,
                       '0' when S1,
                       '1' when S2,
                       '0' when S3,
@@ -115,6 +121,7 @@ with curr_state select
                       'X' when others;
         with curr_state select
             RD_RST <= '1' when S0,
+                      '0' when SX,
                       '0' when S1,
                       '0' when S2,
                       '0' when S3,
@@ -128,6 +135,7 @@ with curr_state select
                       'X' when others;
         with curr_state select
             SUB_EN <= '0' when S0,
+                      '0' when SX,
                       '0' when S1,
                       '0' when S2,
                       '0' when S3,
@@ -141,6 +149,7 @@ with curr_state select
                       'X' when others;
         with curr_state select
            O_MEM_E <= '0' when S0,
+                      '1' when SX,
                       '1' when S1,
                       '0' when S2,
                       '0' when S3,
@@ -154,6 +163,7 @@ with curr_state select
                       'X' when others;
         with curr_state select
           O_MEM_WE <= '0' when S0,
+                      '0' when SX,
                       '0' when S1,
                       '0' when S2,
                       '0' when S3,
@@ -164,5 +174,19 @@ with curr_state select
                       '1' when S8,
                       '0' when S9,
                       '0' when SF,
+                      'X' when others;
+        with curr_state select
+          SEL_ADD <=  '0' when S0,
+                      '0' when SX,
+                      '1' when S1,
+                      '1' when S2,
+                      '1' when S3,
+                      '1' when S4,
+                      '1' when S5,
+                      '1' when S6,
+                      '1' when S7,
+                      '1' when S8,
+                      '1' when S9,
+                      '1' when SF,
                       'X' when others;
 end architecture;
