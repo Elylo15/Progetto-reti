085741132731a5bdf1c7deba91147e9f19d257f7
Elylo <58746092+Elylo15@users.noreply.github.com>
2024-04-04 23:53:08 +0200

    Update RK.vhd

--- Components/RK/RK.vhd
+++ Components/RK/RK.vhd
@@ -6,20 +6,21 @@ entity RK is
 	i_clk : in std_logic;
 	i_rst : in std_logic;
 	SUM_RK: in std_logic_vector(9 downto 0);
-	K_EN: in std_logic;
 	output_RK : out std_logic_vector(9 downto 0)
  ); 
 end RK;
 architecture RK_arch of RK is 
+signal stored_value: std_logic_vector(9 downto 0);
 begin
 	process(i_clk, i_rst)
 	begin
 	--Sequenziale
 		if i_rst = '1' then
-			output_RK <= (others =>'0');
+			stored_value <= (others =>'0');
 		--altrimenti, RD si mantiene invariato
-		elsif i_clk'event and i_clk='1' then
-		  output_RK <= SUM_RK;
+		elsif i_clk' event and i_clk='1' then
+		  stored_value <= SUM_RK;
 		end if;
+		output_RK <= stored_value;
 	end process;
 end RK_arch;
