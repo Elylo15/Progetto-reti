1433b4513dfa5bfb4b7b26c7bf439e9a7d67d78e
Elylo <58746092+Elylo15@users.noreply.github.com>
2024-04-04 19:41:00 +0200

    Update SUB_RC.vhd

--- Components/SUB_RC/SUB_RC.vhd
+++ Components/SUB_RC/SUB_RC.vhd
@@ -6,7 +6,7 @@ entity SUB_RC is
     Port (
         RC: in std_logic_vector(7 downto 0);
         SUB_EN: in std_logic;
-        output: out std_logic_vector(7 downto 0)
+        output_SUM_RC: out std_logic_vector(7 downto 0)
     );
 end SUB_RC;
 
@@ -18,9 +18,9 @@ begin
             A => RC(0),
             B => SUB_EN,
             borrow => s_borrow,
-            diff => output(0)
+            diff => output_SUM_RC(0)
         );
        
-    output(7 downto 1) <= RC(7 downto 1);
+    output_SUM_RC(7 downto 1) <= RC(7 downto 1);
 
 end SUB_RC_arch;
