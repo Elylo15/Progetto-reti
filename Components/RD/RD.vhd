2c07fe96e12e174782015d399a1bf303315a2273
Elylo <58746092+Elylo15@users.noreply.github.com>
2024-04-04 19:40:07 +0200

    Update RD.vhd

--- Components/RD/RD.vhd
+++ Components/RD/RD.vhd
@@ -6,24 +6,24 @@ entity RD is
 	i_clk : in std_logic;
 	RD_RST: in std_logic;
 	RD_EN: in std_logic;
-	output: out std_logic_vector(7 downto 0)
+	output_RD: out std_logic_vector(7 downto 0)
  ); 
 end RD;
 
 architecture RD_arch of RD is
-signal stored_value: std_logic_vector(7 downto 0);
---Dobbiamo tenere a mente il funzionamento del registro, che è un registro PIPO, cioè banalmente stiamo considerando una serie di flip flop in parallelo
+--Dobbiamo tenere a mente il funzionamento del registro, che ? un registro PIPO, cio? banalmente stiamo considerando una serie di flip flop in parallelo
 --quindi per testarlo dobbiamo verificare che al tempo t ci sia un ingresso IN(t) e un uscita OUT(t-1)
 begin
 	memory : process(RD_RST, i_clk)
 	begin 
 		if(RD_RST='1') then
-			stored_value <= (others =>'0');
+			output_RD <= (others =>'0');
 		elsif(i_clk'event and i_clk='1' and RD_EN='1') then
-			stored_value <= i_mem_data;
+			output_RD <= i_mem_data;
 		elsif(i_clk'event and i_clk='1' and RD_EN='0') then
-			stored_value <= stored_value;
+			output_RD <= i_mem_data;
+	    else
+	       output_RD <= (others =>'X');
 		end if;
-		output <= stored_value;
 	end process;
 end RD_arch;
