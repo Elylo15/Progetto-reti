bb7a592e3a46d785d08a52c03f776ea635138b74
Elylo <58746092+Elylo15@users.noreply.github.com>
2024-04-04 21:16:25 +0200

    Update SUB_RC_tb.vhd

--- Components/SUB_RC/SUB_RC_tb.vhd
+++ Components/SUB_RC/SUB_RC_tb.vhd
@@ -10,7 +10,7 @@ architecture sub_RC_tb_arch of sub_RC_tb is
     Port (
         RC: in std_logic_vector(7 downto 0);
         SUB_EN: in std_logic;
-        output: out std_logic_vector(7 downto 0)
+        output_SUM_RC: out std_logic_vector(7 downto 0)
     );
     end component;
     
@@ -23,7 +23,7 @@ begin
     uut: SUB_RC port map(
         RC => s_RC,
         SUB_EN => s_SUB_EN,
-        output => s_output
+        output_SUM_RC => s_output
     );
     
      stim_proc: process
