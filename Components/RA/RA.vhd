436116c0e7c82339c86cd932364c0711236be106
Elylo <58746092+Elylo15@users.noreply.github.com>
2024-04-04 22:50:23 +0200

    Update RA.vhd

--- Components/RA/RA.vhd
+++ Components/RA/RA.vhd
@@ -18,8 +18,6 @@ begin
             output_RA <= (others =>'0');
         elsif (i_clk'event and i_clk='1') then
             output_RA <= mux_RA;
-        else
-            output_RA <= (others =>'X');
         end if; 
     end process;
 end RA_arch;
