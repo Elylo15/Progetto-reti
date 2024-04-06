36942e8a8eb76ee73a620e4d9cd6dc8682632dd2
Elylo <58746092+Elylo15@users.noreply.github.com>
2024-04-06 16:04:45 +0200

    Update RC.vhd

--- Components/RC/RC.vhd
+++ Components/RC/RC.vhd
@@ -6,7 +6,6 @@ entity RC is
         i_clk : in std_logic;
         i_rst : in std_logic;
         SUB_RC: in std_logic_vector(7 downto 0);
-        SUB_EN: in std_logic;
         output_RC : out std_logic_vector(7 downto 0)
     );
 end RC;
@@ -22,7 +21,6 @@ begin
         elsif i_clk' event and i_clk='1' then
             stored_value <= SUB_RC;
         end if;
-    output_RC <= stored_value;
     end process;
-
+     output_RC <= stored_value;
 end RC_arch;
