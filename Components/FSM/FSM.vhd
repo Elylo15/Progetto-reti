614a3cef7fcaf49b10c5c8710b865d2587fa9a84
Elylo <58746092+Elylo15@users.noreply.github.com>
2024-04-06 16:02:18 +0200

    Update FSM.vhd

--- Components/FSM/FSM.vhd
+++ Components/FSM/FSM.vhd
@@ -19,7 +19,8 @@ entity FSM is
         SEL_ADD: out std_logic;
         SUB_EN: out std_logic;
         O_MEM_E: out std_logic;
-        O_MEM_WE: out std_logic
+        O_MEM_WE: out std_logic;
+        DONE_MUX_SEL: out std_logic
     );
 end entity;
 
@@ -41,7 +42,9 @@ begin
                     elsif START='1' then
                         curr_state <= SX;
                     end if;
-
+	-- Una volta arrivati in SX allora significa che start è alto
+	-- Porre una condizione su start è ridondante a meno che tu non ti aspetti che start cambi
+	-- e voglia reagire di conseguenza
                 when SX =>
                     curr_state <= S1;
 
@@ -57,21 +60,21 @@ begin
 
                 when S3 =>
                     curr_state <= S4;
-
+-- Transizione definita solo dalla condizione posta su DONE
                 when S4 =>
                     if START='1' and DONE='1' then
                         curr_state <= SF;
                     else
                         curr_state <= S1;
                     end if;
-
+-- Transizione definita solo dalla condizione posta su DONE
                 when S5 =>
                     if START='1' and DONE='1' then
                         curr_state <= SF;
                     else
                         curr_state <= S6;
                     end if;
-
+-- Transizione definita solo dalla condizione posta su E
                 when S6 =>
                     if START='1' and E='0' then
                         curr_state <= S2;
@@ -91,13 +94,25 @@ begin
                         curr_state <= SF;
                     end if;
 
+                -- when S8 =>
+                --    if DONE='0' then
+		--	if CHECK_ZERO='0' then
+                --       	curr_state <= S6;
+                --    	elsif CHECK_ZERO='1' then
+                --        	curr_state <= S9;
+		--	END if;
+                --    elsif DONE='1' then
+                --        curr_state <= SF;
+                -- end if;
+
+-- Transizione condizionata solo da E
                 when S9 =>
                     if START='1' and E='1' then
                         curr_state <= S7;
                     elsif  START='1' and E='0' then
                         curr_state <= S2;
                     end if;
-
+-- Qui è corretto considerare start poichè è l'unico stato in cui un suo cambiamento cambi il comportamento della tua FSM
                 when SF =>
                     if START='1' and DONE='1' then
                         curr_state <= SF;
@@ -119,6 +134,7 @@ begin
         SUB_EN <='0';
         O_MEM_E <='0';
         O_MEM_WE <='0';
+        DONE_MUX_SEL <= '1';
 
         if curr_state = S0 then
             ADD_EN <='0';
@@ -130,6 +146,7 @@ begin
             O_MEM_E <= '0';
             O_MEM_WE <= '0';
             SEL_ADD <='0';
+            DONE_MUX_SEL <= '0'; 
 
         elsif curr_state = SX then
             ADD_EN <='0';
