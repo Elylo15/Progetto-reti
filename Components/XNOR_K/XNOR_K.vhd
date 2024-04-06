8c5e0fb9f4130a03d01c82fb3924a9018af9665b
Elylo <58746092+Elylo15@users.noreply.github.com>
2024-04-06 16:03:29 +0200

    Update XNOR_K.vhd

--- Components/XNOR_K/XNOR_K.vhd
+++ Components/XNOR_K/XNOR_K.vhd
@@ -3,6 +3,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity XNOR_K is
 	port(
+	    i_rst: in std_logic;
 		i_k: in std_logic_vector(9 downto 0);
 		RK: in std_logic_vector(9 downto 0);
 		output_XNOR_K: out std_logic
@@ -14,8 +15,14 @@ architecture XNOR_K_arch of XNOR_K is
 	signal intermediate: std_logic_vector(9 downto 0);
 	
 begin
-    XNOR_calculation: for i in 0 to 9 generate
-	   intermediate(i) <= (i_k(i) xnor RK(i));
-	end generate;
-	output_XNOR_K <= '1' when intermediate = "1111111111" else '0';
+   process(i_k, RK, i_rst)
+    begin 
+        if i_rst= '1' then
+            output_XNOR_K <='0';
+        elsif i_k = RK then   
+            output_XNOR_K <='1';
+        else
+            output_XNOR_K <= '0';
+        end if; 
+   end process;
 end XNOR_K_arch;
