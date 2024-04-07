b8b40db7dc16f26b627c372cb3d07102ec7d3a1e
Elylo <58746092+Elylo15@users.noreply.github.com>
2024-04-07 17:02:04 +0200

    Update SUB_RC.vhd

--- Components/SUB_RC/SUB_RC.vhd
+++ Components/SUB_RC/SUB_RC.vhd
@@ -17,16 +17,16 @@ begin
         port map (
             a => RC(0),
             b => SUB_EN,
-            c => s_borrow(0),
-            s => output_SUM_RC(0)
+            borrow => s_borrow(0),
+            res => output_SUM_RC(0)
         );
        GEN_HALF_SUB: for i in 1 to 7 generate 
                 HA_i: entity work.half_subtractor
                     port map(
                        a => RC(i),
                        b => s_borrow(i-1),
-                       c => s_borrow(i),
-                       s => output_SUM_RC(i)
+                       borrow => s_borrow(i),
+                       res => output_SUM_RC(i)
             );
             end generate GEN_HALF_SUB;
 
